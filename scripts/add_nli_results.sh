c5b1a15298abd6573510013ea6c8124589a6a70c
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-04-21 19:09:58 +0200

    Fix scripts for pre-commit

--- scripts/add_nli_results.sh
+++ scripts/add_nli_results.sh
@@ -1,1 +1,4 @@
 git add --force nli_results/**/models/*/README.md nli_results/**/models/*/trainer_state.json
+git commit -m "Add NLI results"
+git add --force nli_results/**/models/*/README.md nli_results/**/models/*/trainer_state.json
+git commit -m "Add NLI results"
