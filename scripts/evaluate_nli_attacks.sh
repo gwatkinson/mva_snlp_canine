02b8340553dd037644b34a8f86b51a9afa0b9b6c
Gabriel Watkinson <gabriel.watkinson@gmail.com>
2023-04-22 14:05:49 +0200

    Fix tyo

--- scripts/evaluate_nli_attacks.sh
+++ scripts/evaluate_nli_attacks.sh
@@ -5,33 +5,33 @@ echo "========================"
 echo "Evaluating modified 100k_en"
 echo "------------------------"
 nli_augmented_dataset 100k_en --language_subset 'en,fr,es,bg,ru,de,el'
-nli_visualize_results 100k_en --attacked
+nli_visualise_results 100k_en --attacked
 echo "========================"
 
 
 echo "Evaluating modified 300k_fr"
 echo "------------------------"
 nli_augmented_dataset 300k_all --language_subset 'en,fr,es,bg,ru,de,el'
-nli_visualize_results 300k_all --attacked
+nli_visualise_results 300k_all --attacked
 echo "========================"
 
 
 echo "Evaluating modified 200k_ar_tr"
 echo "------------------------"
 nli_augmented_dataset 200k_ar_tr --language_subset 'ar,tr'
-nli_visualize_results 200k_ar_tr --attacked
+nli_visualise_results 200k_ar_tr --attacked
 echo "========================"
 
 
 echo "Evaluating modified 100k_zh_th"
 echo "------------------------"
 nli_augmented_dataset 100k_zh_th --language_subset 'zh,th,hi,ur,vi'
-nli_visualize_results 100k_zh_th --attacked
+nli_visualise_results 100k_zh_th --attacked
 echo "========================"
 
 
 echo "Evaluating modified 300k_all"
 echo "------------------------"
 nli_augmented_dataset 300k_all --language_subset 'en,fr,es,bg,ru,de,el'
-nli_visualize_results 300k_all --languages 'en, fr, es, bg, ru' --attacked
+nli_visualise_results 300k_all --languages 'en, fr, es, bg, ru' --attacked
 echo "========================"
